a9a1d7f474fe31ca477dadaac3a60e86fa233204
Michael Sloan <mgsloan@gmail.com>
2015-01-26 14:24:25 -0800

    Add sadd, srem, and sismember Redis set commands

--- redis-fp/src/FP/Redis/Command/Set.hs
+++ redis-fp/src/FP/Redis/Command/Set.hs
@@ -6,7 +6,10 @@
 -- See <http://redis.io/commands#set>.
 
 module FP.Redis.Command.Set
-    ( smembers )
+    ( smembers
+    , sadd
+    , srem
+    , sismember )
     where
 
 import ClassyPrelude.Conduit
@@ -19,3 +22,21 @@ import FP.Redis.Types.Internal
 smembers :: Key -> CommandRequest [ByteString]
 smembers key =
     makeCommand "SMEMBERS" [encodeArg key]
+
+-- | Add the specified members to the set stored at key.
+-- See <http://redis.io/commands/sadd>.
+sadd :: Key -> [ByteString] -> CommandRequest Int64
+sadd key members =
+    makeCommand "SADD" (encodeArg key : map encodeArg members)
+
+-- | Remove the specified members from the set stored at key.
+-- See <http://redis.io/commands/srem>.
+srem :: Key -> [ByteString] -> CommandRequest Int64
+srem key members =
+    makeCommand "SREM" (encodeArg key : map encodeArg members)
+
+-- | Returns if member is a member of the set stored at key.
+-- See <http://redis.io/commands/sismember>.
+sismember :: Key -> ByteString -> CommandRequest Bool
+sismember key member =
+    makeCommand "SISMEMBER" [encodeArg key, encodeArg member]
