04e96edcd3821ce8d219f09384aa89cad98f98ef
Michael Sloan <mgsloan@gmail.com>
2015-01-27 18:09:19 -0800

    Add a bunch of RedisQueue / JobQueue documentation

--- work-queue/src/Distributed/JobQueue.hs
+++ work-queue/src/Distributed/JobQueue.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
@@ -34,13 +34,29 @@ import           Network.HostName (getHostName)
 import qualified STMContainers.Map as SM
 import           System.Posix.Process (getProcessID)
 
+-- | Configuration of a 'jobQueueWorker'.
 data WorkerConfig = WorkerConfig
     { workerResponseDataExpiry :: Seconds
+      -- ^ How many seconds the response data should be kept around.
+      -- The longer it's kept around, the more opportunity there is to
+      -- eliminate redundant work, if identical requests are made.  A
+      -- longer expiry also allows more time between sending a
+      -- response notification and the client reading its data.
     }
 
 -- | This is intended for use as the body of the @inner@ function of a
--- master server.  It takes requests encoded as @Vector payload@ and
--- enqueues each item to a 'WorkQueue'.
+-- master server.  It waits for a request encoded as @Vector payload@
+-- and enqueues each item to a 'WorkQueue'.  It also runs
+-- 'sendHeartbeats', in order to handle responding to heartbeat
+-- requests.
+--
+-- Note that currently it will only work on one @Vector payload@
+-- request at a time.  This is a limitation that will be fixed in the
+-- future, if necessary.
+--
+-- Also note that the return type is @void@, indicating that this
+-- function is never intended to return, as it is always waiting for
+-- more work.
 jobQueueWorker
     :: (MonadConnect m, Binary payload)
     => WorkerConfig
@@ -63,13 +79,30 @@ jobQueueWorker config r queue toResult = do
                     subresults <- mapQueue queue (decode (fromStrict request))
                     sendResponse r worker requestId bid =<< toResult subresults
 
+-- | Variables and settings used by 'jobQueueClient' /
+-- 'jobQueueRequest'.
 data ClientVars m = ClientVars
     { clientSubscribed :: TVar Bool
+      -- ^ This is set to 'True' once the client is subscribed to its
+      -- backchannel, and so ready to send reqeusts.
+      -- 'jobQueueRequest' blocks until this is 'True'.
     , clientDispatch :: SM.Map RequestId (ByteString -> m ())
+      -- ^ A map between 'RequestId's and their associated handlers.
+      -- 'jobQueueClient' uses this to invoke the handlers inserted by
+      -- 'jobQueueRequest'.
     , clientHeartbeatCheckIvl :: Seconds
+      -- ^ How often to send heartbeat requests to the workers, and
+      -- check for responses.  This value should be the same for all
+      -- clients.
     , clientInfo :: ClientInfo
+      -- ^ Information about the client needed to invoke the client
+      -- functions in "Distributed.RedisQueue".
     }
 
+-- | Create a new 'ClientVars' value.  This uses a default method of
+-- computing a 'BackChannelId', which combines the host name with the
+-- process ID.  The user must provide values for
+-- 'clientHeartbeatCheckIvl' and 'clientRequestExpiry' as arguments.
 newClientVars :: Seconds -> Seconds -> IO (ClientVars m)
 newClientVars heartbeatCheckIvl requestExpiry = ClientVars
     <$> newTVarIO False
@@ -77,6 +110,13 @@ newClientVars heartbeatCheckIvl requestExpiry = ClientVars
     <*> pure heartbeatCheckIvl
     <*> (ClientInfo <$> getBackchannelId <*> pure requestExpiry)
 
+-- | Runs a listener for responses from workers, which dispatches to
+-- callbacks registered with 'jobQueueRequest'.  It also runs
+-- 'checkHeartbeats', to ensure that some server periodically checks
+-- the worker heartbeats.
+--
+-- This function should be run in its own thread, as it never returns
+-- (the return type is @void@).
 jobQueueClient
     :: MonadConnect m
     => ClientVars m
@@ -104,11 +144,16 @@ jobQueueClient cvs r = do
                     response <- readResponse r requestId
                     handler response
 
+-- | Once a 'jobQueueClient' has been run with the 'ClientVars' value,
+-- this function can be used to make requests and block on their
+-- response.  It's up to the user of this and 'jobQueueWorker' to
+-- ensure that the types of @payload@ match up, and that the
+-- 'ByteString' responses are encoded as expected.
 jobQueueRequest
-    :: (MonadCommand m, MonadLogger m, Binary request)
+    :: (MonadCommand m, MonadLogger m, Binary payload)
     => ClientVars m
     -> RedisInfo
-    -> Vector request
+    -> Vector payload
     -> m ByteString
 jobQueueRequest cvs r request = do
     resultVar <- newEmptyMVar
@@ -120,6 +165,10 @@ jobQueueRequest cvs r request = do
 -- to note is that exceptions thrown by the callback do not get
 -- rethrown.  Instead, they're printed, due to jobQueueClient using
 -- 'FP.Redis.withSubscriptionsWrapped'.
+--
+-- This command does block on the request being enqueued.  First, it
+-- blocks on 'clientSubscribed', then it may also need to wait for the
+-- Redis server to become available.
 jobQueueRequest'
     :: (MonadCommand m, MonadLogger m, Binary request)
     => ClientVars m
--- work-queue/src/Distributed/RedisQueue.hs
+++ work-queue/src/Distributed/RedisQueue.hs
@@ -1,15 +1,17 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ViewPatterns #-}
 
--- | This module provides a work-queue based on Redis, with the
+-- | This module provides a work-queue based on Redis.  It's
+-- recommended that the higher level "Distributed.JobQueue" be used
+-- instead, as it uses this API properly.  The Redis queue has the
 -- following features:
 --
 -- (1) Many clients can enqueue work and block on responses, without
@@ -32,8 +34,15 @@
 --
 -- One caveat is that it does not current make the guarantee that
 -- results are only delivered once.  It's up to the client to deal
--- with this. The higher level API "Distributed.JobQueue" does handle
--- this gracefully.
+-- with this. The higher level "Distributed.JobQueue" handles this
+-- gracefully by deregistering the callback when a response comes
+-- back.
+--
+-- Along with being idempotent, this also requires that computations
+-- be essentially pure.  This is because requests are hashed in order
+-- to figure out their 'RequestId'.  If a response already exists for
+-- a given 'RequestId', then it is returned instead of enqueuing a
+-- redundant computation.
 module Distributed.RedisQueue
     ( ClientInfo(..), WorkerInfo(..), RedisInfo(..)
     , RequestId(..), BackchannelId(..), WorkerId(..)
@@ -82,14 +91,14 @@ data RedisInfo = RedisInfo
 
 -- | ID of a redis channel used for notifications about a particular
 -- request.  One way to use this is to give each client server its own
--- 'BackchannelId', so that it is informed of responses to the
--- requests it makes.
+-- 'BackchannelId', so that it is only informed of responses
+-- associated with the requests it makes.
 newtype BackchannelId = BackchannelId { unBackchannelId :: ByteString }
     deriving (Eq, Show, Binary, IsString)
 
 -- | Every worker server has a 'WorkerId' to uniquely identify it.
 -- It's needed for the fault tolerance portion - in the event that a
--- worker goes down we need to be able to reenqueue its work.
+-- worker goes down we need to be able to re-enqueue its work.
 newtype WorkerId = WorkerId { unWorkerId :: ByteString }
     deriving (Eq, Show, Binary, IsString)
 
@@ -99,12 +108,25 @@ newtype WorkerId = WorkerId { unWorkerId :: ByteString }
 newtype RequestId = RequestId { unRequestId :: ByteString }
     deriving (Eq, Show, Binary, Hashable)
 
+-- | This acquires a Redis 'Connection' and wraps it up along with a
+-- key prefix into a 'RedisInfo' value.  This 'RedisInfo' value can
+-- then be used to run the various functions in this module.
 withRedisInfo
     :: MonadConnect m
     => ByteString -> ConnectInfo -> (RedisInfo -> m ()) -> m ()
 withRedisInfo redisKeyPrefix redisConnectInfo f =
     withConnection redisConnectInfo $ \redisConnection -> f RedisInfo {..}
 
+-- | Pushes a request to the compute workers.  If the result has been
+-- computed previously, and the result is still cached, then it's
+-- returned as a 'Just' value in the 'snd' part of the result.  The
+-- 'RequestId' provided in the result can later be used to associate a
+-- response with the request (see 'subscribeToResponses' and
+-- 'readResponse').
+--
+-- The request data is given an expiry of 'clientRequestExpiry'.  If
+-- the request data expires before being dequeued, then the request is
+-- silently dropped.
 pushRequest
     :: MonadCommand m
     => RedisInfo -> ClientInfo -> ByteString -> m (RequestId, Maybe ByteString)
@@ -122,6 +144,11 @@ pushRequest r (ClientInfo bid expiry) request = do
             run_ r $ lpush (requestsKey r) (toStrict (encode (k, bid)))
             return (k, Nothing)
 
+-- | This function is used by the compute workers to take work off of
+-- the queue.  When work is taken off the queue, it also gets moved to
+-- an in-progress queue specific to the worker, atomically.  This is
+-- done so that in the event of server failure, the work items can be
+-- re-enqueued.
 popRequest
     :: MonadCommand m
     => RedisInfo -> WorkerInfo -> m (RequestId, BackchannelId, Maybe ByteString)
@@ -133,6 +160,9 @@ popRequest r (WorkerInfo wid _) = do
             mx <- run r $ get (requestDataKey r k)
             return (k, bid, mx)
 
+-- | Send a response for a particular request.  This is done by the
+-- compute workers once they're done with the computation, and have
+-- results to send.
 sendResponse
     :: (MonadCommand m, MonadLogger m)
     => RedisInfo -> WorkerInfo -> RequestId -> BackchannelId -> ByteString -> m ()
@@ -158,7 +188,11 @@ sendResponse r (WorkerInfo wid expiry) k bid x = do
     -- above.
     run_ r $ del [requestDataKey r k]
 
--- | Retrieves and deletes the response for the specified 'RequestId'.
+-- | Retrieves the response for the specified 'RequestId'.  This
+-- function is usually called in the body of a 'subscribeToResponses'
+-- handler, in order to fetch the response after notification of its
+-- existence.  It throws a 'KeyMissing' error if there is no response
+-- for the specified 'RequestId'.
 readResponse
     :: MonadCommand m
     => RedisInfo -> RequestId -> m ByteString
@@ -193,6 +227,10 @@ sendHeartbeats r (WorkerInfo wid _) ready = do
   where
     remInactive = run_ r $ srem (inactiveKey r) [unWorkerId wid]
 
+-- | Periodically check worker heartbeats.  This uses
+-- 'periodicActionWrapped' to share the responsibility of checking the
+-- heartbeats amongst multiple client servers.  All invocations of
+-- this should use the same time interval.
 checkHeartbeats
     :: MonadConnect m => RedisInfo -> Seconds -> m void
 checkHeartbeats r ivl =
@@ -251,27 +289,41 @@ getUnusedWorkerId r initial = go (0 :: Int)
 
 -- * Functions to compute Redis keys
 
+-- List of "Data.Binary" encoded @(RequestId, BackchannelId)@.
 requestsKey :: RedisInfo -> Key
 requestsKey  r = Key $ redisKeyPrefix r <> "requests"
 
+-- Given a 'RequestId', computes the key for the request or response
+-- data.
 requestDataKey, responseDataKey :: RedisInfo -> RequestId -> Key
 requestDataKey  r k = Key $ redisKeyPrefix r <> "request:" <> unRequestId k
 responseDataKey r k = Key $ redisKeyPrefix r <> "response:" <> unRequestId k
 
+-- Given a 'BackchannelId', computes the name of the 'Channel'.
 responseChannel :: RedisInfo -> BackchannelId -> Channel
 responseChannel r k = Channel $ redisKeyPrefix r <> "responses:" <> unBackchannelId k
 
+-- Given a 'WorkerId', computes the key of its in-progress list.
 inProgressKey :: RedisInfo -> WorkerId -> Key
 inProgressKey r k = Key $ redisKeyPrefix r <> "in-progress:" <> unWorkerId k
 
 inactiveKey, workersKey, heartbeatFunctioningKey :: RedisInfo -> Key
+-- A set of 'WorkerId' who have not yet removed their keys (indicating
+-- that they're still alive and responding to heartbeats).
 inactiveKey r = Key $ redisKeyPrefix r <> "heartbeat:inactive"
+-- A set of 'WorkerId's that are currently thought to be running.
 workersKey r = Key $ redisKeyPrefix r <> "heartbeat:workers"
+-- Stores a "Data.Binary" encoded 'Bool'.
 heartbeatFunctioningKey r = Key $ redisKeyPrefix r <> "heartbeat:functioning"
 
+-- Channel used for requesting that the workers remove their
+-- 'WorkerId' from the set at 'inactiveKey'.
 heartbeatChannel :: RedisInfo -> Channel
 heartbeatChannel r = Channel $ redisKeyPrefix r <> "heartbeat:channel"
 
+-- Prefix used for the 'periodicActionWrapped' invocation, which is
+-- used to share the responsibility of periodically checking
+-- heartbeats.
 heartbeatTimeKey :: RedisInfo -> ByteString
 heartbeatTimeKey r = redisKeyPrefix r <> "heartbeat:time"
 
@@ -290,6 +342,7 @@ getExisting r k = do
         Nothing -> liftIO $ throwIO (KeyMissing k)
         Just result -> return result
 
+-- Like 'rpush', but takes multiple values.
 rpush' :: Key -> [ByteString] -> CommandRequest Int64
 rpush' key vals = makeCommand "RPUSH" (encodeArg key : map encodeArg vals)
 
