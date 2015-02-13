b62018b4275b95e350c97e171cecb15b0fb8726f
Michael Sloan <mgsloan@gmail.com>
2015-02-13 04:03:28 -0800

    Factor out commonality in job queue tests

--- work-queue/test/Distributed/JobQueueSpec.hs
+++ work-queue/test/Distributed/JobQueueSpec.hs
@@ -4,7 +4,7 @@
 module Distributed.JobQueueSpec where
 
 import ClassyPrelude
-import Control.Concurrent.Async (withAsync)
+import Control.Concurrent.Async (withAsync, race)
 import Control.Concurrent.Lifted (threadDelay, fork, killThread)
 import Control.Monad.Logger (runStdoutLoggingT)
 import Control.Monad.Trans.Control (control)
@@ -21,55 +21,61 @@ import Test.Hspec (Spec, it, shouldBe)
 
 spec :: Spec
 spec = do
-    it "Runs enqueued computations" $ do
-        clearRedisKeys
-        runResourceT $ do
-            _ <- forkWorker "redis" 0
-            _ <- forkWorker "redis" 0
-            resultVar <- forkDispatcher
+    jqit "Runs enqueued computations" $ do
+        _ <- forkWorker "redis" 0
+        _ <- forkWorker "redis" 0
+        resultVar <- forkDispatcher
+        checkResult resultVar 5 (Just 0)
+    jqit "Doesn't lose data when master fails" $ do
+        -- This worker will take the request and become a master.
+        master <- forkWorker "redis" 0
+        resultVar <- forkDispatcher
+        threadDelay (100 * 1000)
+        -- Expect no results, because there are no slaves.
+        noResult <- tryTakeMVar resultVar
+        liftIO $ do
+            noResult `shouldBe` Nothing
+            cancelProcess master
+            putStrLn "=================================="
+            putStrLn "Master cancelled"
+            putStrLn "=================================="
+        -- One of these will become a master and one will become a
+        -- slave.
+        --
+        -- This also tests the property that if there are stale
+        -- slave requests, they get ignored because the connection
+        -- fails.
+        _ <- forkWorker "redis" 0
+        _ <- forkWorker "redis" 0
+        checkResult resultVar 10 (Just 0)
+    jqit "Long tasks complete" $ do
+        _ <- forkWorker "redis-long" 0
+        _ <- forkWorker "redis-long" 0
+        resultVar <- forkDispatcher
+        checkResult resultVar 15 (Just 0)
+    jqit "Non-existent slave request is ignored" $ do
+        -- Enqueue an erroneous slave request
+        enqueueSlaveRequest (SlaveRequest "localhost" 1337)
+        -- One worker will become a master, the other will get the
+        -- erroneous slave request.
+        _ <- forkWorker "redis" 0
+        _ <- forkWorker "redis" 0
+        resultVar <- forkDispatcher
+        checkResult resultVar 5 (Just 0)
+    jqit "preserves data despite slaves being started and killed periodically" $ do
+        resultVar <- forkDispatcher
+        let randomSlaveSpawner = forever $ do
+                pid <- forkWorker
+                ms <- randomRIO (150, 300)
+                threadDelay (1000 * ms)
+                cancelProcess pid
+        void $ randomSlaveSpawner `race`
+            randomSlaveSpawner `race`
             checkResult resultVar 5 (Just 0)
-    it "Doesn't lose data when master fails" $ do
-        clearRedisKeys
-        runResourceT $ do
-            -- This worker will take the request and become a master.
-            master <- forkWorker "redis" 0
-            resultVar <- forkDispatcher
-            threadDelay (100 * 1000)
-            -- Expect no results, because there are no slaves.
-            noResult <- tryTakeMVar resultVar
-            liftIO $ do
-                noResult `shouldBe` Nothing
-                cancelProcess master
-                putStrLn "=================================="
-                putStrLn "Master cancelled"
-                putStrLn "=================================="
-            -- One of these will become a master and one will become a
-            -- slave.
-            --
-            -- This also tests the property that if there are stale
-            -- slave requests, they get ignored because the connection
-            -- fails.
-            _ <- forkWorker "redis" 0
-            _ <- forkWorker "redis" 0
-            checkResult resultVar 10 (Just 0)
-    it "Long tasks complete" $ do
-        clearRedisKeys
-        runResourceT $ do
-            _ <- forkWorker "redis-long" 0
-            _ <- forkWorker "redis-long" 0
-            resultVar <- forkDispatcher
-            checkResult resultVar 15 (Just 0)
-    it "Non-existent slave request is ignored" $ do
-        clearRedisKeys
-        runResourceT $ do
-            -- Enqueue an erroneous slave request
-            enqueueSlaveRequest (SlaveRequest "localhost" 1337)
-            -- One worker will become a master, the other will get the
-            -- erroneous slave request.
-            _ <- forkWorker "redis" 0
-            _ <- forkWorker "redis" 0
-            resultVar <- forkDispatcher
-            checkResult resultVar 5 (Just 0)
+
+
+jqit :: String -> ResourceT IO () -> Spec
+jqit name f = it name $ clearRedisKeys >> runResourceT f
 
 forkDispatcher :: ResourceT IO (MVar Int)
 forkDispatcher = do
