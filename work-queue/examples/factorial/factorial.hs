65cf141e1212dfa62a957dda3445fad347e96337
Michael Sloan <mgsloan@gmail.com>
2015-03-06 15:48:00 -0800

    Use atomicModifyIORef' in factorial example

--- work-queue/examples/factorial/factorial.hs
+++ work-queue/examples/factorial/factorial.hs
@@ -1,10 +1,9 @@
 module Main where
 
-import           Control.Applicative
 import           Control.Concurrent.MVar (newEmptyMVar, takeMVar, putMVar)
 import           Control.Concurrent.STM (atomically)
 import           Control.Monad
-import           Data.IORef (newIORef, modifyIORef', readIORef)
+import           Data.IORef (newIORef, atomicModifyIORef', readIORef)
 import           Data.WorkQueue (queueItem)
 import qualified Distributed.WorkQueue as WQ
 
@@ -28,10 +27,10 @@ main = do
     results <- newIORef []                      -- Results sent back to the master go into this list.
     doneMvar <- newEmptyMVar                    -- Put in () when all done
 
-    let processResult i res = do                -- What the master does when receiving a result:
-          modifyIORef' results ((i,res):)       -- * Append to results list
-          n <- length <$> readIORef results     -- * When we have all results,
-          when (n == 100) $ putMVar doneMvar () -- *   put a () into `doneMvar`
+    let processResult i res = do                           -- What the master does when receiving a result:
+          xs' <- atomicModifyIORef' results $              -- * Append to results list, atomically, since these
+              \xs -> let xs' = (i,res) : xs  in (xs', xs') --   callbacks sometimes run in parallel.
+          when (length xs' == 100) $ putMVar doneMvar ()   -- * When we have all results, put a () into `doneMvar`
 
     forM_ [1..100] $ \i ->                             -- Schedule all the work
       atomically $ queueItem queue i (processResult i) -- and specify the callback for each result.
