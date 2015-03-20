a72c2940bb9bed0420b41847f0239eb0f82f91e7
Michael Sloan <mgsloan@gmail.com>
2015-03-20 13:05:19 -0700

    Fix STM masking test and demonstrate STM unmasking
    Thanks to @bitonic for figuring out why the old test was behaving
    differently than I expected.

--- work-queue/test/Control/Exception/MaskSpec.hs
+++ work-queue/test/Control/Exception/MaskSpec.hs
@@ -5,8 +5,7 @@ module Control.Exception.MaskSpec (spec) where
 import Control.Concurrent (threadDelay)
 import Control.Concurrent.Async
 import Control.Concurrent.STM
-import Control.Exception (mask, uninterruptibleMask)
--- import Control.Exception.Mask
+import Control.Exception.Mask
 import Test.Hspec
 
 spec :: Spec
@@ -25,15 +24,34 @@ spec = do
         result <- poll thread
         show result `shouldBe` "Nothing"
         -- This fails to cancel the thread, because it's masked.
-        threadDelay (10 * 1000)
-        cancel thread
+        _ <- async $ cancel thread
         threadDelay (10 * 1000)
         result <- poll thread
-        -- Oddly enough, this is actually
-        -- Just (Left thread blocked indefinitely in an STM transaction)
         show result `shouldBe` "Nothing"
         -- Setting the var to True causes it to exit.
         atomically $ writeTVar var True
         threadDelay (10 * 1000)
         result <- poll thread
-        show result `shouldBe` "Just (Right ())"
+        show result `shouldBe` "Just (Left thread killed)"
+    -- Demonstrates the need for this.
+    it "STM can now be unmasked inside an 'atomically' block!" $ do
+        ready <- newTVarIO False
+        var <- newTVarIO False
+        thread <- async $ uninterruptibleMask $ \ra -> do
+            atomically $ writeTVar ready True
+            atomically $ restoreSTM ra $ check =<< readTVar var
+        -- Wait for the thread to enter the masked block.
+        atomically $ check =<< readTVar ready
+        -- Check that it is indeed blocked.
+        threadDelay (10 * 1000)
+        result <- poll thread
+        show result `shouldBe` "Nothing"
+        -- This succeeds cancelling the thread, because it's unmasked
+        -- within the transaction.
+        threadDelay (10 * 1000)
+        _ <- async $ cancel thread
+        threadDelay (10 * 1000)
+        result <- poll thread
+        show result `shouldBe` "Just (Left thread killed)"
+        -- Reference to var, so that STM isn't blocked indefinitely.
+        atomically $ writeTVar var True
