83c30f3225279e365eeec2f0dc604d8e9ec604ed
Michael Sloan <mgsloan@gmail.com>
2015-04-15 17:57:47 -0700

    Derive Data / Typeable / Generic for more types

--- redis-fp/src/FP/Redis/Mutex/Types.hs
+++ redis-fp/src/FP/Redis/Mutex/Types.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude, OverloadedStrings, ScopedTypeVariables, TypeFamilies,
              DeriveDataTypeable, FlexibleContexts, FlexibleInstances, RankNTypes, GADTs,
-             ConstraintKinds, NamedFieldPuns, GeneralizedNewtypeDeriving #-}
+             ConstraintKinds, NamedFieldPuns, GeneralizedNewtypeDeriving, DeriveGeneric #-}
 
 -- | Redis mutex types.
 
@@ -13,23 +13,24 @@ module FP.Redis.Mutex.Types
 
 import ClassyPrelude.Conduit
 import FP.Redis.Types
+import Data.Data (Data)
 
 -- | Mutex token, used to identify the owner of a mutex.
 newtype MutexToken = MutexToken ByteString
-    deriving (Show)
+    deriving (Eq, Show, Data, Typeable, Generic)
 
 -- | Key which is known to refer to a "FP.Redis.Mutex"
 newtype MutexKey = MutexKey { unMutexKey :: Key }
-    deriving (Eq, Show, Ord, IsString)
+    deriving (Eq, Show, Ord, IsString, Data, Typeable, Generic)
 
 -- | Exception thrown by "FP.Redis.Mutex".
 data RedisMutexException
     = IncorrectRedisMutexException
-    deriving (Show, Typeable)
+    deriving (Show, Data, Typeable, Generic)
 instance Exception RedisMutexException
 
 -- | Prefix used for constructing keys for use by
 -- 'FP.Redis.Mutex.periodicActionWrapped' /
 -- 'FP.Redis.Mutex.periodicActionEx'.
 newtype PeriodicPrefix = PeriodicPrefix { unPeriodicPrefix :: ByteString }
-    deriving (Eq, Show, Ord, IsString)
+    deriving (Eq, Show, Ord, IsString, Data, Typeable, Generic)
