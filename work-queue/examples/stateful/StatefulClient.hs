531610bc75b65b4baa57d2535b52c09babf0746e
Michael Sloan <mgsloan@gmail.com>
2016-01-28 20:01:20 -0800

    Fix compilation of hpc-example-stateful-client
    Not sure how that happened, I think maybe due to just loading it into
    ghci and not testing "stack build"

--- work-queue/examples/stateful/StatefulClient.hs
+++ work-queue/examples/stateful/StatefulClient.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Control.Concurrent.STM
+import Control.Monad
 import Control.Monad.Logger
 import Data.ByteString (ByteString)
+import Data.ByteString.Char8 (pack)
+import Data.Maybe
+import Data.Time
 import Distributed.JobQueue.Client.NewApi
-import Distributed.RedisQueue
 
 type Request = ByteString
 type Response = ByteString
@@ -15,9 +18,11 @@ main = do
   jc <- newJobClient logFunc defaultJobClientConfig { jccRedisPrefix = "stateful-demo:" }
   -- Use the current time as the request id, to avoid cached results.
   now <- getCurrentTime
-  let rid = RequestId (show now)
+  let rid = RequestId (pack (show now))
   let request = ", request input " :: Request
-  stmResponse <- submitRequest jc rid request
+  mresp <- submitRequest jc rid request
+  when (isJust mresp) $ fail "Didn't expect cached result"
+  stmResponse <- waitForResponse jc rid
   eres <- atomically $ do
     meres <- stmResponse
     case meres of
