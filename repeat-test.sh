eaff58a6ccfafe839117c4e367cfbab224f71c88
Michael Sloan <mgsloan@gmail.com>
2016-01-28 20:18:04 -0800

    Update repeat-test.sh script with new test name

--- repeat-test.sh
+++ repeat-test.sh
@@ -9,11 +9,11 @@ stack test $1 --no-run-tests
 if [ $? -ne 0 ]
   then exit
 fi
-$1/$DIST/build/test/test -m "$2"
+$1/$DIST/build/wq-test/wq-test -m "$2"
 while [ $? -eq 0 ]; do
   echo "**** Iteration #$ITER ****"
   ITER=$((ITER+1))
   rm -r logs # Clear out thread-file-logger logs
   killall -9 test # Kill old work-queue test processes
-  $1/$DIST/build/test/test -m "$2"
+  $1/$DIST/build/wq-test/wq-test -m "$2"
 done
