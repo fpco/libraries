9227db53b1401d226f995d55f8554e5ad52743eb
Francesco Mazzoli <f@mazzo.li>
2016-05-06 14:37:12 +0200

    redis-fp: Use resource-pool, restore fuzz test

--- redis-fp/src/FP/Redis/Managed.hs
+++ redis-fp/src/FP/Redis/Managed.hs
@@ -15,24 +15,15 @@ module FP.Redis.Managed
     ) where
 
 import ClassyPrelude
-import Control.Monad.Logger
-import qualified Control.Concurrent.STM as STM
-import qualified Data.Text.Encoding as T
+import Control.Monad.Trans.Control
+import Control.Monad.Trans.Unlift
+import qualified Data.Pool as Pool
 
 import FP.Redis.Connection
 import FP.Redis.Types.Internal
 
--- REVIEW TODO add timestamp recording last time the connection was touched and "reaper"
--- to disconnect stale ones
-
-data Connections = Connections
-    { connectionsActive :: !Int
-    , connectionsAvailable :: ![Connection]
-    }
-
 data ManagedConnection = ManagedConnection
-    { mcConnections :: !(TVar Connections)
-    , mcMaxConnections :: !Int
+    { mcPool :: !(Pool.Pool Connection)
     , mcConnectInfo :: !ConnectInfo
     }
 
@@ -44,78 +35,26 @@ newManagedConnection ::
 newManagedConnection cinfo maxConns = do
     when (maxConns < 1) $
         liftIO (fail ("newManagedConnection: maxConns must be >= 1, but got " ++ show maxConns))
-    $logDebug ("ManagedConnection creating initial connection to " ++ showConnectInfo)
-    conn <- connect cinfo
-    conns <- liftIO (newTVarIO (Connections 1 [conn]))
-    return ManagedConnection{mcConnections = conns, mcConnectInfo = cinfo, mcMaxConnections = maxConns}
-  where
-    showConnectInfo = T.decodeUtf8 (connectHost cinfo) ++ ":" ++ tshow (connectPort cinfo)
+    unlift <- askUnliftBase
+    pool <- liftIO $ Pool.createPool
+        (unliftBase unlift (connect cinfo))
+        (\conn -> unliftBase unlift (disconnect conn))
+        1
+        (fromIntegral (100 :: Int)) -- ^ Keep it for 100 secs
+        maxConns
+    return ManagedConnection{mcPool = pool, mcConnectInfo = cinfo}
 
-destroyManagedConnection :: (MonadCommand m) => ManagedConnection -> m ()
-destroyManagedConnection conn = do
-    mapM_ disconnect . connectionsAvailable =<< atomically (readTVar (mcConnections conn))
+destroyManagedConnection :: (MonadIO m) => ManagedConnection -> m ()
+destroyManagedConnection = liftIO . Pool.destroyAllResources . mcPool
 
 withManagedConnection :: (MonadConnect m) => ConnectInfo -> Int -> (ManagedConnection -> m a) -> m a
-withManagedConnection cinfo maxConns = bracket (newManagedConnection cinfo maxConns) destroyManagedConnection
+withManagedConnection cinfo maxConns =
+    bracket (newManagedConnection cinfo maxConns) destroyManagedConnection
 
 managedConnectInfo :: ManagedConnection -> ConnectInfo
 managedConnectInfo = mcConnectInfo
 
 useConnection :: forall m a.
-       (MonadConnect m)
+       (MonadBaseControl IO m)
     => ManagedConnection -> (Connection -> m a) -> m a
-useConnection mc cont =
-    bracket tryTakeConnection putConnection $ \(_wasThere, mbConnRef) -> do
-        let fillConnRef = do
-                conn <- connect (mcConnectInfo mc)
-                writeIORef mbConnRef (Just conn)
-                return conn
-        (conn, fresh) <- do
-            mbConn <- readIORef mbConnRef
-            case mbConn of
-                Just conn -> return (conn, False)
-                Nothing -> do
-                    $logDebug ("ManagedConnection creating new connection to " ++ showConnectInfo)
-                    conn <- fillConnRef
-                    return (conn, True)
-        mbErr :: Either IOError a <- try (cont conn)
-        -- Retry once if the connection is not fresh.
-        case (fresh, mbErr) of
-            (False, Left err) -> do
-                $logWarn ("Encountered IOError " ++ tshow err ++ " when trying to use a connection to " ++ showConnectInfo ++ ", trying to reconnect since the connection might be stale.")
-                -- Disconnect just to be safe (IOErrors are ignored)
-                disconnect conn
-                -- Retry once
-                conn' <- fillConnRef
-                res <- cont conn'
-                return res
-            (True, Left err) -> throwM err
-            (_, Right res) -> return res
-  where
-    showConnectInfo = T.decodeUtf8 (connectHost (mcConnectInfo mc)) ++ ":" ++ tshow (connectPort (mcConnectInfo mc))
-
-    tryTakeConnection :: m (Bool, IORef (Maybe Connection))
-    tryTakeConnection = do
-        mbConn <- atomically $ do
-            Connections{..} <- readTVar (mcConnections mc)
-            case connectionsAvailable of
-                conn : conns -> do
-                    writeTVar (mcConnections mc) (Connections connectionsActive conns)
-                    return (Just conn)
-                [] -> if mcMaxConnections mc > connectionsActive
-                    then return Nothing
-                    else STM.retry
-        (isJust mbConn, ) <$> newIORef mbConn
-
-    putConnection :: (Bool, IORef (Maybe Connection)) -> m ()
-    putConnection (wasThere, connRef) = do
-        mbConn <- readIORef connRef
-        atomically $ do
-            Connections{..} <- readTVar (mcConnections mc)
-            case mbConn of
-                Just conn -> let
-                    active = connectionsActive + if wasThere then 0 else 1
-                    in if active > mcMaxConnections mc
-                        then return ()
-                        else writeTVar (mcConnections mc) (Connections active (conn : connectionsAvailable))
-                Nothing -> return ()
+useConnection mc = Pool.withResource (mcPool mc)
--- redis-fp/test/Fuzz.hs
+++ redis-fp/test/Fuzz.hs
@@ -4,59 +4,48 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
+import ClassyPrelude
 import Test.Hspec (it, hspec)
 import System.Random
-import qualified Data.Vector.Mutable as MV
 import FP.Redis (Connection, VKey(..), Key(..))
 import qualified Data.Map as M
 import qualified FP.Redis as R
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BS8
 import Control.Monad.Trans.Control
-import Control.Exception (bracket)
 import Control.Monad.Trans.Reader (ReaderT(..))
-import Control.Monad.Reader.Class (MonadReader, asks)
-import Control.Concurrent.Lifted (fork)
-import Data.Monoid ((<>))
 import qualified Data.List.NonEmpty as LNE
-import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Monad (replicateM, when, void)
-import Control.Monad.Logger (runStdoutLoggingT)
-import Data.Traversable (forM)
-import Data.Foldable (mapM_, forM_)
-import Prelude hiding (mapM, mapM_)
-import Data.IORef
-import Data.Int (Int64)
-import Data.Maybe (fromMaybe)
+import Control.Monad.Logger (runStdoutLoggingT, LoggingT, MonadLogger)
 import Control.Monad.Base (MonadBase)
-import Control.Concurrent.MVar.Lifted
+import Prelude (read)
+import qualified Control.Concurrent.Async.Lifted.Safe as Async
+import Control.Monad.Reader (asks)
 
 main :: IO ()
-main = hspec $ it "passes test using incr and get commands" fuzzTest
+main = hspec $ do
+  it "passes test using incr and get commands (light)" (fuzzTest 5 100 100)
+  it "passes test using incr and get commands (heavy)" (fuzzTest 50 1000 1000)
 
-fuzzTest :: IO ()
-fuzzTest = runFuzzM 5 $ do
+fuzzTest :: Int -> Int -> Int -> IO ()
+fuzzTest maxConns threads runs = runFuzzM maxConns $ do
   clearRedisKeys
   -- Fork 100 threads, and have each perform 100 actions
-  doneVars <- forM [1..(100 :: Int)] $ \_ -> do
-    done <- newEmptyMVar
-    void $ fork $ do
-      forM_ [1..100] $ \(_ :: Int) -> do
-        which <- liftIO randomIO
-        if which
-          then incr =<< selectOrGenerateKey
-          else getAndCheck =<< selectOrGenerateKey
-      putMVar done ()
-    return done
-  mapM_ takeMVar doneVars
+  void $ flip Async.mapConcurrently [1..(threads :: Int)] $ \_ -> do
+    forM_ [1..runs] $ \(_ :: Int) -> do
+      which <- liftIO randomIO
+      void $ if which
+        then incr =<< selectOrGenerateKey
+        else getAndCheck =<< selectOrGenerateKey
 
 -- Implementation of FuzzM
 
-newtype FuzzM a = FuzzM { unFuzzM :: ReaderT State IO a }
-  deriving (Functor, Applicative, Monad, MonadBase IO, MonadReader State, MonadIO)
+newtype FuzzM a = FuzzM { unFuzzM :: ReaderT State (LoggingT IO) a }
+  deriving (Functor, Applicative, Monad, MonadBase IO, MonadReader State, MonadIO, MonadMask, MonadLogger, MonadCatch, MonadThrow)
 
 instance MonadBaseControl IO FuzzM where
   type StM FuzzM a = a
@@ -66,8 +55,7 @@ instance MonadBaseControl IO FuzzM where
 data State = State
   { stateRedis :: IORef (M.Map Key Value)
     -- ^ State of redis key-value store.
-  , stateConns :: MV.IOVector (Maybe Connection)
-    -- ^ Redis connections
+  , stateConn :: R.ManagedConnection
   }
 
 -- For now, all values are Ints that support incrementing.
@@ -76,38 +64,39 @@ data Value = Value
   , incrsInFlight :: Int64
   }
 
+instance Semigroup Value where
+  Value v1 i1 <> Value v2 i2 = Value (v1 + v2) (i1 + i2)
+
 instance Monoid Value where
   mempty = Value 0 0
-  mappend (Value v1 i1) (Value v2 i2) = Value (v1 + v2) (i1 + i2)
+  mappend = (<>)
 
 minPossibleValue, maxPossibleValue :: Value -> Int64
 minPossibleValue = curValue
 maxPossibleValue v = curValue v + incrsInFlight v
 
-runFuzzM :: Int -> FuzzM a -> IO a
-runFuzzM maxConns f = do
+runFuzzM :: forall a. Int -> FuzzM a -> IO a
+runFuzzM maxConns f = runStdoutLoggingT $ do
   stateRedis <- newIORef M.empty
-  stateConns <- MV.replicate maxConns Nothing
-  runReaderT (unFuzzM f) (State {..})
+  R.withManagedConnection (R.connectInfo "localhost") maxConns $ \stateConn ->
+    runReaderT (unFuzzM f) (State {..})
 
 incr :: Key -> FuzzM Int64
 incr key = do
   mpRef <- asks stateRedis
-  conn <- getConnection
-  liftIO $ bracket
+  bracket
     (atomicModifyIORef' mpRef $ \mp ->
       let mp' = M.insertWithKey' (\_ -> (<>)) key (Value 0 1) mp
        in (mp', ()))
     (\() -> atomicModifyIORef' mpRef $ \mp ->
       let mp' = M.insertWithKey' (\_ -> (<>)) key (Value 1 (-1)) mp
        in (mp', ()))
-    (\() -> R.runCommand conn $ R.incr (VKey key))
+    (\() -> withConnection (\conn -> R.runCommand conn (R.incr (VKey key))))
 
 getAndCheck :: Key -> FuzzM Int64
 getAndCheck key = do
-  conn <- getConnection
   before <- readValue key
-  actual <- maybe 0 (read . BS8.unpack) <$> liftIO (R.runCommand conn (R.get (VKey key)))
+  actual <- maybe 0 (read . BS8.unpack) <$> withConnection (\conn -> R.runCommand conn (R.get (VKey key)))
   after <- readValue key
   let lower = minPossibleValue before
       upper = maxPossibleValue after
@@ -150,21 +139,13 @@ randomShortBS = do
 testPrefix :: BS.ByteString
 testPrefix = "redis-fp.fuzz."
 
-getConnection :: FuzzM Connection
-getConnection = do
-  conns <- asks stateConns
-  liftIO $ do
-    ix <- randomRIO (0, MV.length conns - 1)
-    mconn <- MV.read conns ix
-    case mconn of
-      Nothing -> do
-        conn <- runStdoutLoggingT $ R.connect (R.connectInfo "localhost")
-        MV.write conns ix (Just conn)
-        return conn
-      Just conn -> return conn
+withConnection :: (Connection -> FuzzM a) -> FuzzM a
+withConnection cont = do
+  conn <- asks stateConn
+  R.useConnection conn cont
 
 clearRedisKeys :: FuzzM ()
 clearRedisKeys = do
-  redis <- getConnection
-  matches <- liftIO $ R.runCommand redis $ R.keys (testPrefix <> "*")
-  liftIO $ mapM_ (R.runCommand_ redis . R.del) (LNE.nonEmpty matches)
+  withConnection $ \redis -> do
+    matches <- liftIO $ R.runCommand redis $ R.keys (testPrefix <> "*")
+    liftIO $ mapM_ (R.runCommand_ redis . R.del) (LNE.nonEmpty matches)
