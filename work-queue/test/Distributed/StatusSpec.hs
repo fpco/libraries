b2269e5b4d5a6b419c7d78934b3e40039741777b
Philipp Kant <philipp@fpcomplete.com>
2016-06-08 18:07:02 +0200

    Fix tests for Distributed.Status
    - clearing database after every test
    - increased wait time;
      The tests initiate actions that should change the status, wait a short
      time, and check the status.  It seems that Travis needs more time for
      the changes in the status to take effect.

--- work-queue/test/Distributed/StatusSpec.hs
+++ work-queue/test/Distributed/StatusSpec.hs
@@ -12,10 +12,10 @@ import           Control.Concurrent.MVar
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Control.Monad.Logger
-import           Data.Store.TypeHash (mkManyHasTypeHash)
 import           Data.ByteString (ByteString)
 import           Data.ByteString.Char8 (pack)
 import           Data.Maybe
+import           Data.Store.TypeHash (mkManyHasTypeHash)
 import           Distributed.Heartbeat
 import           Distributed.JobQueue.Client
 import           Distributed.JobQueue.Status
@@ -49,76 +49,76 @@ waitingWorkerFunc mvar _ _ _ = do
 runWithoutLogs :: LoggingT IO a -> IO a
 runWithoutLogs = runStdoutLoggingT . filterLogger (\_ _ -> False)
 
-
 waitForHeartbeat :: IO ()
 waitForHeartbeat = threadDelay $
     3 * 1000 * 1000 * (fromIntegral . unSeconds $ testHeartbeatCheckIvl)
-
 waitShortInterval :: IO ()
-waitShortInterval = threadDelay $ 500 * 1000
+waitShortInterval = threadDelay $ 2 * 1000 * 1000
 
-    
-queueRequests :: MonadConnect m => Redis -> m ()
-queueRequests redis = do
-    -- send requests
-    _ <- liftIO $ mapM startClient reqs
-    liftIO waitShortInterval
-    -- check that both getAllRequests and the list of pending requests match the submitted jobs
-    jqs <- getJobQueueStatus redis
-    jqsPending jqs `shouldMatchList` map fst reqs
-    allReqs <- getAllRequests redis
-    allReqs `shouldMatchList` map fst reqs
-  where reqs = [(RequestId . pack . show $ n, pack . show $ n) | n <- [0..10 :: Int]]
+someRequests :: [(RequestId, ByteString)]
+someRequests = [(RequestId . pack . show $ n, pack . show $ n) | n <- [0..10 :: Int]]
 
-addWorker :: MonadConnect m => Redis -> m ()
-addWorker redis = do
-    jqs <- getJobQueueStatus redis
-    mvar <- liftIO newEmptyMVar
-    _ <- liftIO $ waitingWorker mvar
-    liftIO waitShortInterval
-    jqs' <- getJobQueueStatus redis
-    length (jqsWorkers jqs') `shouldBe` length (jqsWorkers jqs) + 1
-    length (jqsPending jqs') `shouldBe` max 0 (length (jqsPending jqs) - 1)
+queueReqs :: MonadConnect m => [(RequestId, Request)] -> m ()
+queueReqs reqs = liftIO $ do
+    mapM_ startClient reqs
+    waitShortInterval
 
-heartbeatFailure :: MonadConnect m => Redis -> m ()
-heartbeatFailure redis = do
-    jqs <- getJobQueueStatus redis
+failHeartbeat :: MonadConnect m => m ()
+failHeartbeat = do
     mvar <- liftIO newEmptyMVar
     worker <- liftIO $ waitingWorker mvar
-    liftIO waitShortInterval
+    liftIO waitForHeartbeat
     liftIO $ Async.cancel worker
     liftIO waitForHeartbeat
-    -- we should now have a heartbeat failure
-    jqs' <- getJobQueueStatus redis
-    length (jqsHeartbeatFailures jqs') `shouldBe` length (jqsHeartbeatFailures jqs) + 1
 
-completeJob :: MonadConnect m => Redis -> m ()
-completeJob redis = do
-    stats <- getAllRequestStats redis
+queueRequestsTest :: MonadConnect m => Redis -> m ()
+queueRequestsTest redis = do
+    queueReqs someRequests
+    -- check that both getAllRequests and the list of pending requests match the submitted jobs
+    jqs <- getJobQueueStatus redis
+    jqsPending jqs `shouldMatchList` map fst someRequests
+    allReqs <- getAllRequests redis
+    allReqs `shouldMatchList` map fst someRequests
+
+addWorkerTest :: MonadConnect m => Redis -> m ()
+addWorkerTest redis = do
+    queueReqs someRequests
     mvar <- liftIO newEmptyMVar
     _ <- liftIO $ waitingWorker mvar
-    liftIO $ putMVar mvar ()
+    liftIO waitShortInterval
+    jqs' <- getJobQueueStatus redis
+    length (jqsWorkers jqs') `shouldBe` 1
+    length (jqsPending jqs') `shouldBe` length someRequests - 1
+
+heartbeatFailureTest :: MonadConnect m => Redis -> m ()
+heartbeatFailureTest redis = do
+    queueReqs someRequests
+    failHeartbeat
+    jqs' <- getJobQueueStatus redis
+    length (jqsHeartbeatFailures jqs') `shouldBe` 1
+
+completeJobTest :: MonadConnect m => Redis -> m ()
+completeJobTest redis = do
+    queueReqs someRequests
+    mvar <- liftIO newEmptyMVar
+    _ <- liftIO $ waitingWorker mvar
+    liftIO $ putMVar mvar () -- the worker should finish the job now.
     liftIO waitShortInterval
     stats' <- getAllRequestStats redis
     length (filter (isJust . rsComputeFinishTime . snd) stats')
-        `shouldBe`
-        length (filter (isJust . rsComputeFinishTime . snd) stats) + 1
+        `shouldBe` 1
 
-clearFailures :: MonadConnect m => Redis -> m ()
-clearFailures redis = do
+clearFailuresTest :: MonadConnect m => Redis -> m ()
+clearFailuresTest redis = do
+    queueReqs someRequests
     clearHeartbeatFailures redis
     jqs <- getJobQueueStatus redis
     length (jqsHeartbeatFailures jqs) `shouldBe` 0
 
--- | Version of 'redisIt' that does not clear the database.
-redisIt' :: String -> (forall m. (MonadConnect m) => Redis -> m ()) -> Spec
-redisIt' msg cont = loggingIt msg $
-    withRedis testRedisConfig cont
-
 spec :: Spec
 spec = do
-    flakyTest $ redisIt "reports queued requests" queueRequests
-    flakyTest $ redisIt' "reports added worker and enqueued job" addWorker
-    flakyTest $ redisIt' "reports heartbeat failure" heartbeatFailure
-    flakyTest $ redisIt' "reports completed jobs" completeJob
-    flakyTest $ redisIt' "clearing the list of heartbeat failures works" clearFailures
+    redisIt "reports queued requests" queueRequestsTest
+    redisIt "reports added worker and enqueued job" addWorkerTest
+    redisIt "reports heartbeat failure" heartbeatFailureTest
+    redisIt "reports completed jobs" completeJobTest
+    redisIt "clearing the list of heartbeat failures works" clearFailuresTest
