d907de701b87d1c74e79af5943af8d3ad2e2a1ea
Philipp Kant <philipp@fpcomplete.com>
2016-07-25 14:42:41 +0200

    Benchmark: separate processes for workers.
    This is closer to what we do in practice.

--- work-queue/examples/performance/Main.hs
+++ work-queue/examples/performance/Main.hs
@@ -11,7 +11,7 @@
 {-# LANGUAGE RecordWildCards #-}
 
 import           Control.DeepSeq
-import           Control.Exception (evaluate)
+import           Control.Exception (evaluate, bracket)
 import           Control.Monad (forM)
 import           Control.Monad.IO.Class
 import           Control.Monad.Logger
@@ -31,6 +31,12 @@ import           Distributed.Stateful.Master
 import           FP.Redis (MonadConnect)
 import           System.Environment (getArgs)
 import qualified System.IO.ByteBuffer as BB
+import           System.Environment (getExecutablePath)
+import Distributed.JobQueue.Client
+import Distributed.JobQueue.Worker
+import qualified Data.Conduit.Network as CN
+import Control.Concurrent.Mesosync.Lifted.Safe
+import System.Process
 
 type Request = [State]
 type Response = Double
@@ -48,7 +54,7 @@ $(mkManyHasTypeHash [ [t| ByteString |]
                     ])
 
 vlength :: Int
-vlength = 100
+vlength = 1000
 
 roundtrip :: forall a m . (MonadConnect m, Store a) => a -> m a
 roundtrip x = BB.with (Just $ 64 * 1024) $ \bb -> do
@@ -66,8 +72,7 @@ myUpdate context input (!v1, !v2) = do
             result = V.sum prod
             v1' = V.map (*(i*V.sum input)) v1
             v2' = V.map (*(context*i)) $ V.zipWith (/) v1' v2
-            state' = (v1', v2')
---        state' <- liftIO . evaluate $ result `deepseq` v1' `deepseq` v2' `deepseq` (v1', v2')
+        state' <- liftIO . evaluate $ result `deepseq` v1' `deepseq` v2' `deepseq` (v1', v2')
         return (state', result)
     return $! foldl' (\((v1,v2),x) ((v1',v2'),x') -> ((V.zipWith (+) v1 v1',V.zipWith (+) v2 v2'),x+x')) ((V.replicate vlength 0, V.replicate vlength 0),0) foo
 
@@ -82,13 +87,12 @@ masterArgs = MasterArgs
 myStates :: [State]
 myStates =
     let r = mkStdGen 42
-    in concat $ mapM (const (evalRandT go r)) [1..2000 :: Int]
+    in concat $ mapM (const (evalRandT go r)) [1..200 :: Int]
   where
     go :: RandT StdGen [] State
     go = do
         v1 <- V.generateM vlength $ \_ -> getRandomR (0,1)
         v2 <- V.generateM vlength $ \_ -> getRandomR (0,1)
-        -- v1 `seq` v2 `seq` return $ State v1 v2
         v1 `deepseq` v2 `deepseq` return (v1, v2)
 
 -- | We'll use the same input over and over again.
@@ -117,20 +121,53 @@ myAction req mh = do
                         ) [1..5::Int]
     return (sum $ sum <$> (head finalStates :: HashMap StateId (HashMap StateId Output)))
 
-doRequest :: Int -> String -> IO ()
-doRequest nSlaves mode = do
-    t0 <- getTime
-    res <- logErrors $ case mode of
-        "stm" -> runSimplePureStateful masterArgs nSlaves (myAction myStates)
-        "nm" -> runSimpleNMStateful "127.0.0.1" masterArgs nSlaves (myAction myStates)
-        _ -> error "Arguments should be <nSlaves> [stm|nm]"
-    t1 <- getTime
-    putStrLn $ "The request took " ++ show (t1 - t0) ++ " seconds."
-    print res
+-- * functionality to spawn workers in separate processed, similar as in the dpf.
 
+spawnAndWaitForWorker :: Int -> IO ProcessHandle
+spawnAndWaitForWorker numSlaves = do
+  program <- getExecutablePath
+  runProcess program [show numSlaves, "nmSlave"] Nothing Nothing Nothing Nothing Nothing
+
+jqc :: JobQueueConfig
+jqc = defaultJobQueueConfig "performance:"
+
+performRequest :: IO ()
+performRequest = logErrors $ withJobClient jqc $ \jc -> do
+    t0 <- liftIO getTime
+    rid <- uniqueRequestId
+    submitRequest jc rid myStates
+    mRes <- waitForResponse_ jc rid
+    t1 <- liftIO getTime
+    liftIO . putStrLn $ "The request took " ++ show (t1 - t0) ++ " seconds."
+    liftIO $ print (mRes :: Maybe Response)
 
 main :: IO ()
 main = do
-    [nSlaves0, mode] <- getArgs
     initializeTime
-    doRequest (read nSlaves0) mode
+    args <- getArgs
+    print args
+    case args of
+        [numSlaves, "nmSlave"] -> do -- spawn nm slave
+            putStrLn "spawning worker"
+            let ss = CN.serverSettings 3333 "*"
+            logErrors $ runJobQueueStatefulWorker
+                jqc
+                ss
+                "127.0.0.1"
+                Nothing
+                masterArgs
+                (NMStatefulMasterArgs (Just $ read numSlaves) (Just $ read numSlaves) (1000 * 1000))
+                (\mh _rid req -> DontReenqueue <$> myAction req mh)
+        [numSlaves, "nm"] -> do -- run calculation
+            bracket
+                (mapConcurrently (const (spawnAndWaitForWorker (read numSlaves))) [0..(read numSlaves :: Int)])
+                (mapM terminateProcess)
+                (\_ -> performRequest)
+        [numSlaves, "stm"] -> do
+            t0 <- getTime
+            res <- logErrors $
+                runSimplePureStateful masterArgs (read numSlaves) (myAction myStates)
+            t1 <- getTime
+            putStrLn $ "The request took " ++ show (t1 - t0) ++ " seconds."
+            print res
+        _ -> putStrLn "Wrong arguments. Arguments should be <nSlaves> [stm|nm]"
